d7a22117a563e6632d3f4561743199ed1e8412e4
BigBlase <thebigblase@gmail.com>
2022-12-12 14:24:28 +0100

    working now

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 SRC = main.cpp
 OBJ = ${SRC:.c=.o}
 OUT = build
+RM = rm 
 
 CC=  /usr/bin/g++
 CFLAGS 	= -ansi -Wall -std=c++17
@@ -13,4 +14,7 @@ build:
 	$(CC) $(SRC) $(CFLAGS) -o main.o
 
 run: $(SRC)
-		$(CC) $(SRC) $(CFLAGS) -o main.o && main.o
+		$(CC) $(SRC) $(CFLAGS) -o main.o && ./main.o
+	
+clean: 
+	$(RM) main.o out.graphml
--- main.cpp
+++ main.cpp
@@ -7,9 +7,6 @@
 #include <random>
 
 typedef boost::property<boost::edge_weight_t, int> EdgeWeightProperty;
-typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, 
-				boost::no_property, EdgeWeightProperty > DirectedGraph;
-typedef boost::graph_traits<DirectedGraph>::edge_iterator edge_iterator;
 typedef boost::adjacency_matrix<boost::undirectedS,
 				boost::property< boost::vertex_color_t, std::string >,
         boost::property< boost::edge_weight_t, int >> MatrixGraph;
@@ -20,9 +17,9 @@ int main(int argc, char * argv[]){
 	std::mt19937 gen(rd());
 	std::uniform_real_distribution<> unif(0, 1);//uniform distribution between 0 and 1
 
-	float const p {0.5};
-	int const numberOfGraph {0};
-	int const n {100};
+	float const p {0.01};
+	int const numberOfGraph {1};
+	int const n {1000};
 
 
 	MatrixGraph g(n);
@@ -31,10 +28,8 @@ int main(int argc, char * argv[]){
 
 	for(auto k {0} ; k < numberOfGraph ; k++){
 		for(auto v {0} ; v < n ; v++){
-			for(auto w {0} ; v < n ; v++){
+			for(auto w {0} ; w < n ; w++){
 				if(p < unif(gen)){
-					//edgeMatrix[w][v] ++;
-					//edgeMatrix[v][w] ++;
 					add_edge(w, v, g);
 				}
 			}
@@ -43,20 +38,18 @@ int main(int argc, char * argv[]){
 
 	boost::dynamic_properties dp;
 
-
-
 	boost::graph_traits<MatrixGraph>::edge_iterator e, e_end;
     for (boost::tie(e,e_end) = edges(g); e != e_end; ++e)
-        put(boost::edge_weight_t(), g, *e, 0);
+        put(boost::edge_weight_t(), g, *e, 1);
 
-  boost::graph_traits<MatrixGraph>::vertex_iterator v, v_end;
+
+	boost::graph_traits<MatrixGraph>::vertex_iterator v, v_end;
     for (boost::tie(v,v_end) = vertices(g); v != v_end; ++v)
         put(boost::vertex_color_t(), g, *v, std::to_string(*v));
 
 	dp.property("weight", get(boost::edge_weight_t(), g));
 	dp.property("node_id", get(boost::vertex_color_t(), g));
 
-	std::ofstream f("/tmp/test");
-	boost::write_graphml(std::cout, g, dp, true);
-
+	std::ofstream f("out.graphml");
+	boost::write_graphml(f, g, dp, true);
 }
