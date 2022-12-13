160f718ee7ed0bf4539b77b90164f571e5d84f5d
Pourquoi? <ryan1991@hotmail.fr>
2022-12-13 18:47:24 +0100

    Degree Frequency

--- main.cpp
+++ main.cpp
@@ -5,51 +5,136 @@
 #include <array>
 #include <string>
 #include <random>
+#include <chrono>
+#include <ctime>
+#include <map>
 
-typedef boost::property<boost::edge_weight_t, int> EdgeWeightProperty;
-typedef boost::adjacency_matrix<boost::undirectedS,
+struct Chrono
+{
+	std::chrono::steady_clock::time_point begin;
+	Chrono()
+	{
+		begin = std::chrono::steady_clock::now();
+	}
+	~Chrono()
+	{
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 1000.f << std::endl;
+	}
+};
+
+
+typedef boost::adjacency_matrix<
+				boost::undirectedS,
 				boost::property< boost::vertex_color_t, std::string >,
         boost::property< boost::edge_weight_t, int >> MatrixGraph;
 
-int main(int argc, char * argv[]){
 
+struct Frequency
+{
+	int degree = 0;
+};
+
+int main(int argc, char * argv[]){
+	Chrono C;
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::uniform_real_distribution<> unif(0, 1);//uniform distribution between 0 and 1
 
-	float const p {0.01};
-	int const numberOfGraph {1};
-	int const n {1000};
+	int const n = 10000;
+	
+	MatrixGraph G(n);
+	const std::string name = "ABCDEF";
 
+	std::map<int, int> distrib;
 
-	MatrixGraph g(n);
-
-	//std::array<std::array<int, n>, n> edgeMatrix {0};
-
-	for(auto k {0} ; k < numberOfGraph ; k++){
-		for(auto v {0} ; v < n ; v++){
-			for(auto w {0} ; w < n ; w++){
-				if(p < unif(gen)){
-					add_edge(w, v, g);
-				}
+	double p = 0.05;
+	
+	for (int v = 0; v < n; v++) {
+		for (int w = 0; w < n; w++) {
+			if (p > unif(gen) && w != v) {
+				boost::add_edge(w, v, G);
 			}
 		}
 	}
-
-	boost::dynamic_properties dp;
-
-	boost::graph_traits<MatrixGraph>::edge_iterator e, e_end;
-    for (boost::tie(e,e_end) = edges(g); e != e_end; ++e)
-        put(boost::edge_weight_t(), g, *e, 1);
-
+	/*
+	boost::print_vertices(G, name.c_str());
+	boost::print_edges(G, name.c_str());*/
+	//boost::print_graph(G);
 
 	boost::graph_traits<MatrixGraph>::vertex_iterator v, v_end;
-    for (boost::tie(v,v_end) = vertices(g); v != v_end; ++v)
-        put(boost::vertex_color_t(), g, *v, std::to_string(*v));
 
-	dp.property("weight", get(boost::edge_weight_t(), g));
-	dp.property("node_id", get(boost::vertex_color_t(), g));
+	for (boost::tie(v, v_end) = boost::vertices(G); v != v_end; ++v)
+	{
+		int degree = boost::out_degree(*v, G);
+		//std::cout << *v << " " << boost::out_degree(*v, G) << std::endl;
 
-	std::ofstream f("out.graphml");
-	boost::write_graphml(f, g, dp, true);
+		distrib[degree] += 1;
+	}
+
+	std::cout << "------------" << std::endl;
+
+	std::string nameFile = "./FrequencyDegree/" + std::to_string(p) + ".json";
+
+	std::ofstream FrenquencyDegree;
+	FrenquencyDegree.open(nameFile);
+
+	if (FrenquencyDegree.is_open())
+	{
+		std::cout << "ok" << std::endl;
+	}
+
+	FrenquencyDegree << "[";
+	
+	for (const auto& it : distrib)
+	{
+		std::cout << it.first << "-" << it.second << std::endl;
+
+		FrenquencyDegree << "{\"degree\":" << std::to_string(it.first) << ",\"freq\":" << std::to_string(it.second) << "}," << std::endl;
+	}
+	long pos = FrenquencyDegree.tellp();
+	FrenquencyDegree.seekp(pos - 1);
+	FrenquencyDegree.write("", 1);
+	FrenquencyDegree << "]";
+	
+	FrenquencyDegree.close();
+
+	/*
+	for(double p = 0.01; p < 1; p += 0.01)
+	{
+		Chrono c;
+		MatrixGraph g(n);
+		int numberOfLinks = 0;
+		for(int v = 0; v < n ; v++){
+			for(int w = 0; w < n ; w++){
+				if(p > unif(gen) && w!=v){
+				add_edge(w, v, g);
+				numberOfLinks++;
+				}
+			}
+		}
+
+		std::cout << p << " : " << numberOfLinks << std::endl;
+		
+		//boost::print_graph(g);
+		
+		boost::dynamic_properties dp;
+
+		boost::graph_traits<MatrixGraph>::edge_iterator e, e_end;
+		for (boost::tie(e,e_end) = edges(g); e != e_end; ++e)
+			put(boost::edge_weight_t(), g, *e, 1);
+
+
+		boost::graph_traits<MatrixGraph>::vertex_iterator v, v_end;
+		for (boost::tie(v,v_end) = vertices(g); v != v_end; ++v)
+			put(boost::vertex_color_t(), g, *v, std::to_string(*v));
+
+		dp.property("weight", get(boost::edge_weight_t(), g));
+		dp.property("node_id", get(boost::vertex_color_t(), g));
+
+		std::string nameOutput = std::to_string(p) + ".graphml";
+
+		std::ofstream f(nameOutput);
+		boost::write_graphml(f, g, dp, true);
+	}*/
 }
