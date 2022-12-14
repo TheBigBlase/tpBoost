88d705a83081fb80a86c0134b6820686f9627a42
Pourquoi? <ryan1991@hotmail.fr>
2022-12-14 18:30:49 +0100

    Ok sa maarch mdrrr

--- main.cpp
+++ main.cpp
@@ -12,14 +12,16 @@
 struct Chrono
 {
 	std::chrono::steady_clock::time_point begin;
-	Chrono()
+	std::string m_name;
+	Chrono(const std::string& name)
 	{
+		m_name = name;
 		begin = std::chrono::steady_clock::now();
 	}
 	~Chrono()
 	{
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-		std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 1000.f << std::endl;
+		std::cout << m_name <<  " :" << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 1000.f << std::endl;
 	}
 };
 
@@ -36,68 +38,83 @@ struct Frequency
 };
 
 int main(int argc, char * argv[]){
-	Chrono C;
+	
+	// Random
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::uniform_real_distribution<> unif(0, 1);//uniform distribution between 0 and 1
 
 	int const n = 10000;
 	
-	MatrixGraph G(n);
+	
 	const std::string name = "ABCDEF";
 
-	std::map<int, int> distrib;
 
-	double p = 0.05;
-	
-	for (int v = 0; v < n; v++) {
-		for (int w = 0; w < n; w++) {
-			if (p > unif(gen) && w != v) {
-				boost::add_edge(w, v, G);
+	for (double p = 0.01; p <= 1; p += 0.09)
+	{
+		std::cout << "------------" << std::endl;
+		Chrono C("Total");
+		std::map<int, int> distrib;
+		MatrixGraph G(n);
+
+		for (int v = 0; v < n; v++) {
+			for (int w = 0; w < n; w++) {
+				if (p > unif(gen) && w != v) {
+					boost::add_edge(w, v, G);
+				}
 			}
 		}
+
+		// BAGATATA
+
+		Chrono c("Babidji");
+		boost::graph_traits<MatrixGraph>::vertex_iterator v, v_end;
+
+		for (boost::tie(v, v_end) = boost::vertices(G); v != v_end; ++v)
+		{
+			int degree = boost::out_degree(*v, G);
+			//std::cout << *v << " " << boost::out_degree(*v, G) << std::endl;
+
+			distrib[degree] += 1;
+		}
+
+		Chrono x("AAAAAAA");
+
+		std::string nameFile = "./FrequencyDegree/" + std::to_string(p) + ".json";
+
+		std::ofstream FrenquencyDegree;
+		FrenquencyDegree.open(nameFile);
+
+		if (FrenquencyDegree.is_open())
+		{
+			std::cout << "ok" << std::endl;
+		}
+
+		std::string* json = new std::string;
+
+		//FrenquencyDegree << "[";
+		*json = "[";
+
+
+		for (const auto& it : distrib)
+		{
+			//std::cout << it.first << "-" << it.second << std::endl;
+
+			//FrenquencyDegree << "{\"degree\":" << std::to_string(it.first) << ",\"freq\":" << std::to_string(it.second) << "},\n";
+			*json += ("{\"degree\":" + std::to_string(it.first) + ",\"freq\":" + std::to_string(it.second) + "},\n");
+		}
+
+		json->pop_back();
+		json->pop_back();
+
+		*json += "]";
+
+		FrenquencyDegree << *json;
+
+		FrenquencyDegree.close();
 	}
-	/*
-	boost::print_vertices(G, name.c_str());
-	boost::print_edges(G, name.c_str());*/
-	//boost::print_graph(G);
 
-	boost::graph_traits<MatrixGraph>::vertex_iterator v, v_end;
-
-	for (boost::tie(v, v_end) = boost::vertices(G); v != v_end; ++v)
-	{
-		int degree = boost::out_degree(*v, G);
-		//std::cout << *v << " " << boost::out_degree(*v, G) << std::endl;
-
-		distrib[degree] += 1;
-	}
-
-	std::cout << "------------" << std::endl;
-
-	std::string nameFile = "./FrequencyDegree/" + std::to_string(p) + ".json";
-
-	std::ofstream FrenquencyDegree;
-	FrenquencyDegree.open(nameFile);
-
-	if (FrenquencyDegree.is_open())
-	{
-		std::cout << "ok" << std::endl;
-	}
-
-	FrenquencyDegree << "[";
 	
-	for (const auto& it : distrib)
-	{
-		std::cout << it.first << "-" << it.second << std::endl;
-
-		FrenquencyDegree << "{\"degree\":" << std::to_string(it.first) << ",\"freq\":" << std::to_string(it.second) << "}," << std::endl;
-	}
-	long pos = FrenquencyDegree.tellp();
-	FrenquencyDegree.seekp(pos - 1);
-	FrenquencyDegree.write("", 1);
-	FrenquencyDegree << "]";
-	
-	FrenquencyDegree.close();
 
 	/*
 	for(double p = 0.01; p < 1; p += 0.01)
